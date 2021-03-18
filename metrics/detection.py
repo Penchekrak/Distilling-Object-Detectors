0a539c7ce3d8be7e9c34217e32eb2c97c043765f
Andrey Spiridonov <spiand97@gmail.com>
2021-03-18 20:10:17 +0300

    added some metric evaluation

--- metrics/detection.py
+++ metrics/detection.py
@@ -2,23 +2,7 @@ import torch
 from torchmetrics import Metric
 from torch import Tensor
 
-
-def to_corner_parametrization(
-        bounding_box: Tensor
-) -> Tensor:
-    """
-    Converts from center/side-lengths parametrization to corners coordinates
-    :param bounding_box: tensor [N, 4] of bounding boxes parameters in the form
-    [x-coordinate of center, y-coordinate of center, width, height]
-    :return: tensor [N, 4] of bounding box parameters in the form
-    [x-coordinate of bottom left corner, y-coordinate of bottom left corner,
-     x-coordinate of top right corner, y-coordinate of top right corner]
-    """
-    center_x, center_y, width, height = bounding_box.T
-    half_w, half_h = width / 2, height / 2
-    bottom_left_x, bottom_left_y = center_x - half_w, center_y - half_h
-    top_right_x, top_right_y = center_x + half_w, center_y + half_h
-    return torch.column_stack((bottom_left_x, bottom_left_y, top_right_x, top_right_y))
+from utils.boxes import to_corner_parametrization
 
 
 def bounding_box_iou(
---
+++ tests/metrics/test_detection.py
@@ -0,0 +1,9 @@
+import pytest
+from metrics.detection import bounding_box_iou
+from utils.boxes import to_corner_parametrization
+
+center_parametrized_boxes = [
+
+]
+
+def test_to_corner_parametrization():
